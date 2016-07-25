d6128c230d67669cbc46d6d8f2ccab541b07bf40
sethvincent <sethvincent@gmail.com>
2016-07-25 13:51:33 -0700

    ignore based on .npmignore or .gitignore or ignore all node_modules

--- commands/deploy.js
+++ commands/deploy.js
@@ -3,6 +3,7 @@ var zlib = require('zlib')
 var path = require('path')
 var tar = require('tar-fs')
 var exit = require('exit')
+var ignore = require('ignore-file')
 
 var staticland = require('../index')
 var addhttps = require('../lib/add-https')
@@ -18,7 +19,12 @@ module.exports = {
     var server = addhttps(args.server || login.server)
     var api = staticland({ server: server })
     var token = login.token
-    var tarstream = tar.pack(source)
+
+    var filter = ignore.sync(path.join(source, '.npmignore'))
+      || ignore.sync(path.join(source, '.gitignore'))
+      || ignore.compile(path.join(source, 'node_modules'))
+
+    var tarstream = tar.pack(source, { ignore: filter })
     var headers = { domain: domain, authorization: `Bearer ${token}` }
 
     api.deploy(tarstream, headers, function (err, res, body) {
